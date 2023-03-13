5422d34f7c53c8dd110c5e1ba5d7fac07e6cbce5
zaftnotameni <mozair.carmo+zaft@gmail.com>
2023-03-13 14:23:46 +0100

    explosion and bomb sounds

--- godot-man/godot/bomb/bomb.gd
+++ godot-man/godot/bomb/bomb.gd
@@ -2,3 +2,14 @@ extends Node2D
 
 var power := 2
 var countdown := 3
+
+@onready var placed_sound := $bomb_placed_sound
+
+func _ready() -> void:
+  placed_sound.play()
+  var tween := create_tween()
+  tween.set_trans(Tween.TRANS_BOUNCE)
+  tween.set_ease(Tween.EASE_IN)
+  tween.set_loops(countdown)
+  tween.tween_property(self, 'scale', Vector2(0.75, 0.75), 0.85)
+  tween.tween_property(self, 'scale', Vector2(1.05, 1.05), 0.15)
--- godot-man/godot/bomb/explosion.gd
+++ godot-man/godot/bomb/explosion.gd
@@ -3,7 +3,15 @@ extends Node2D
 var power := 2
 var countdown := 3
 
+@onready var explosion_center := $explosion_center
+@onready var explosion_sound := $explosion_sound
+
 signal on_explosion_started()
 
+func _ready() -> void:
+  explosion_center.hide()
+
 func start_explosion():
+  explosion_sound.play()
+  explosion_center.show()
   on_explosion_started.emit()
