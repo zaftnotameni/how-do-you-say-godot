cc8105d943fdc96cb47ec1a11e16d06fd6386cad
zaftnotameni <mozair.carmo+zaft@gmail.com>
2023-03-15 18:47:33 +0100

    experimental: rounded ends of explosions

--- godot-man/godot/bomb/explosion/explosion_line.gd
+++ godot-man/godot/bomb/explosion/explosion_line.gd
@@ -10,12 +10,12 @@ func show_explosion():
 func stop_explosion_at(point : Vector2, data : TileData = null) -> void:
   if (stopped_explosion): return
   stopped_explosion = true
-  var p := to_local(point).round()
+  var p := to_local(point).round() - Vector2(0, 0.5)
   var dir = explosion_path.get_point_position(0).direction_to(explosion_path.get_point_position(1))
   var pp = dir * XX.tile_size if data != null else Vector2.ZERO
   explosion_path.set_point_position(1, p + pp)
 
 func set_power(power : int) -> void:
   var original_point := explosion_path.get_point_position(1)
-  var target_point := original_point * power
+  var target_point := original_point * (power - 0.5)
   explosion_path.set_point_position(1, target_point)
